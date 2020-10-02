d094240ab2bad414aec8e9599f81f54f657b35b1
Leo Qiu <qwleo1112@gmail.com>
2020-10-02 01:14:16 -0700

    fixed spacing and indenting

--- src/Components/Table/HomeTable.js
+++ src/Components/Table/HomeTable.js
@@ -1,38 +1,38 @@
 import React from 'react';
 import { data } from '../../data';
 import './HomeTable.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function HomeTable() {
-    return (
-        <div>
-            <table>
-                <caption>{data.data.metadata.title}</caption>
-                <thead className="colnames">
-                    <tr>
-                        <td>Place</td>
-                        <td>Magnitude</td>
-                        <td>Time</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.data.features.map(function (feature) {
-                        var time_val = new Date(feature.properties.time).toISOString();
-                        time_val = time_val.substring(0,time_val.length-5)+"-07:00";
-                        const detail_redir = { 
-                            pathname: "/Detail", 
-                            obj: feature
-                          };
-                        return <tr key={feature.id}>
-                            <td><Link to={detail_redir}>{feature.properties.place}</Link></td>
-                            <td style={{textAlign: "center"}}>{feature.properties.mag}</td>  
-                            <td >{time_val}</td> 
-                        </tr>
-                    })}
-                </tbody>
-            </table>
-        </div>
-    );
+	return (
+		<div>
+			<table>
+				<caption>{data.data.metadata.title}</caption>
+				<thead className="colnames">
+					<tr>
+						<td>Place</td>
+						<td>Magnitude</td>
+						<td>Time</td>
+					</tr>
+				</thead>
+				<tbody>
+					{data.data.features.map(function (feature) {
+						var time_val = new Date(feature.properties.time).toISOString();
+						time_val = time_val.substring(0, time_val.length - 5) + "-07:00";
+						const detail_redir = {
+							pathname: "/Detail",
+							obj: feature
+						};
+						return <tr key={feature.id}>
+							<td><Link to={detail_redir}>{feature.properties.place}</Link></td>
+							<td style={{ textAlign: "center" }}>{feature.properties.mag}</td>
+							<td >{time_val}</td>
+						</tr>
+					})}
+				</tbody>
+			</table>
+		</div>
+	);
 }
 
 export default HomeTable;
--- src/Components/Table/ProfileTable.js
+++ src/Components/Table/ProfileTable.js
@@ -4,47 +4,47 @@ import './ProfileTable.css';
 
 var Sally = data.profile;
 function ProfileTable() {
-    return (
-        // used css grid instead, had troubles getting image alignment with htmnl table
-        <div>
-            <h1>Profile</h1>
-            <div className="grid-container">
-                <div className="grid-item pic">
-                    <img className="profilepic" alt="avatar" src={Sally.avatarImage}></img>
-                </div>
-                <div className="grid-item fname">
-                    <span className="bolded">first name</span>
-                </div>
-                <div className="grid-item fname_val">
-                    <span>{Sally.firstName}</span>
-                </div>
-                <div className="grid-item lname">
-                    <span className="bolded">last name</span>
-                </div>
-                <div className="grid-item lname_val">
-                    <span>{Sally.lastName}</span>
-                </div>
-                <div className="grid-item phone">
-                    <span className="bolded">phone</span>
-                </div>
-                <div className="grid-item phone_val">
-                    <span>{Sally.phone}</span>
-                </div>
-                <div className="grid-item email">
-                    <span className="bolded">email</span>
-                </div>
-                <div className="grid-item email_val">
-                    <span>{Sally.email}</span>
-                </div>
-                <div className="grid-item bio">
-                    <span className="bolded">bio</span>
-                </div>
-                <div className="grid-item bio_val">
-                    <span>{Sally.bio}</span>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		// used css grid instead, had troubles getting image alignment with htmnl table
+		<div>
+			<h1>Profile</h1>
+			<div className="grid-container">
+				<div className="grid-item pic">
+					<img className="profilepic" alt="avatar" src={Sally.avatarImage}></img>
+				</div>
+				<div className="grid-item fname">
+					<span className="bolded">first name</span>
+				</div>
+				<div className="grid-item fname_val">
+					<span>{Sally.firstName}</span>
+				</div>
+				<div className="grid-item lname">
+					<span className="bolded">last name</span>
+				</div>
+				<div className="grid-item lname_val">
+					<span>{Sally.lastName}</span>
+				</div>
+				<div className="grid-item phone">
+					<span className="bolded">phone</span>
+				</div>
+				<div className="grid-item phone_val">
+					<span>{Sally.phone}</span>
+				</div>
+				<div className="grid-item email">
+					<span className="bolded">email</span>
+				</div>
+				<div className="grid-item email_val">
+					<span>{Sally.email}</span>
+				</div>
+				<div className="grid-item bio">
+					<span className="bolded">bio</span>
+				</div>
+				<div className="grid-item bio_val">
+					<span>{Sally.bio}</span>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 
